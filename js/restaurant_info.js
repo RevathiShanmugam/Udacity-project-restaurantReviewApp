30778ae4a393774a5e05785b38aa1d582cc64188
Revathi Shanmugam <srevathikarthik@gmail.com>
2019-01-27 08:38:30 -0800

    Updating MapBox API

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'RREVIEWAPI',
+        mapboxToken: 'pk.eyJ1IjoicmV2YXRoaXMiLCJhIjoiY2pyYzRkdG00MThtNTN5cDhha2ZzY3NnMCJ9.kVwb6rKVUWTAMskZzRRW_A',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
