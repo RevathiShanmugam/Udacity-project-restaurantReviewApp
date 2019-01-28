ecf7968180d1609b357975c525f254de8e7bd8aa
Revathi Shanmugam <srevathikarthik@gmail.com>
2019-01-28 14:53:45 -0800

    Service worker registration

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ let restaurants,
   cuisines
 var newMap
 var markers = []
+/**
+*Registering Service Worker*
+**/
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('./sw.js').then(function() {
+		console.log('Registration worked!');
+	}).catch(function() {
+    console.log('Registration failed!');
+  });
+}
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
