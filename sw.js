35d6c3a2439e8da245a523146c13a165952101d3
Revathi Shanmugam <srevathikarthik@gmail.com>
2019-01-28 15:26:44 -0800

    Install Service worker

--- sw.js
+++ sw.js
@@ -1,1 +1,32 @@
-console.log('SW file');
+/*if the cache name object doesn’t exist,
+create one with the passed cache name*/
+const myCacheName;
+const cacheFiles=[
+				'./',
+				'./index.html',
+				'./restaurant.html',
+				'./css/styles.css',
+				'./data/restaurants.json',
+				'./js/dbhelper.js',
+				'./js/main.js',
+				'./js/restaurant_info.js',
+				'./js/sw_register.js',
+				'./img/1.jpg',
+				'./img/2.jpg',
+				'./img/3.jpg',
+				'./img/4.jpg',
+				'./img/5.jpg',
+				'./img/6.jpg',
+				'./img/7.jpg',
+				'./img/8.jpg',
+				'./img/9.jpg',
+				'./img/10.jpg'
+			];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+  		caches.open('myCacheName').then(function(cache) {
+  			return cache.addAll(cacheFiles);
+      }
+    ))
+});
